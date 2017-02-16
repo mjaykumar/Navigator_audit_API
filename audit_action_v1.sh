9ae9c798733eedfa1222b619c4d08da2e1c07647
Mrityunjay Kumar <rushtomrityunjay@gmail.com>
2017-02-16 12:47:58 -0500

    Update audit_action_v1.sh

--- audit_action_v1.sh
+++ audit_action_v1.sh
@@ -8,17 +8,17 @@ echo "Audit start time in ms epoch:HR is ---  $previous_date_epoch:$previous_dat
 echo "Audit end time in ms epoch:HR is --- $current_date_epoch:$current_date"
 #Audit for Grant Privilege
 # Using flag --noproxy to bypass http proxy and -o for saving output to file
-/usr/bin/curl  --noproxy '*' -k -u jay.kumar:India@123 "https://br1paprhdd11.passporthealth.com:7187/api/v9/audits/?query=command%3D%3DGRANT_PRIVILEGE&startTime=
+/usr/bin/curl  --noproxy '*' -k -u jay.kumar:Password@123 "https://<nav_server>:7187/api/v9/audits/?query=command%3D%3DGRANT_PRIVILEGE&startTime=
 previous_date_epoch&endTime=$current_date_epoch&offset=0&format=$report_format&attachment=false" -o audit_grant_`date +%F`.out
 #Audits for Revoke Privilege
-/usr/bin/curl --noproxy '*' -k -u jay.kumar:India@123 "https://br1paprhdd11.passporthealth.com:7187/api/v9/audits/?query=command%3D%3DREVOKE_PRIVILEGE&startTime=
+/usr/bin/curl --noproxy '*' -k -u jay.kumar:Password@123 "https://<nav_server>:7187/api/v9/audits/?query=command%3D%3DREVOKE_PRIVILEGE&startTime=
 previous_date_epoch&endTime=$current_date_epoch&offset=0&format=$report_format&attachment=false" -o audit_revoke_`date +%F`.out
 
 # Audit for Hue Logins
-/usr/bin/curl --noproxy '*' -k -u jay.kumar:India@123 "https://br1paprhdd11.passporthealth.com:7187/api/v9/audits/?query=command%3D%3DUSER_LOGIN&startTime=$previ
+/usr/bin/curl --noproxy '*' -k -u jay.kumar:Password@123 "https://<nav_server>:7187/api/v9/audits/?query=command%3D%3DUSER_LOGIN&startTime=$previ
 us_date_epoch&endTime=$current_date_epoch&offset=0&format=$report_format&attachment=false" -o  audit_hue_logins_`date +%F`.out
 #Audit for new HDFS directory creation
-/usr/bin/curl --noproxy '*' -k -u jay.kumar:India@123 "https://br1paprhdd11.passporthealth.com:7187/api/v9/audits/?query=command%3D%3Dmkdirs&startTime=$previous_
+/usr/bin/curl --noproxy '*' -k -u jay.kumar:Password@123 "https://<nav_server>:7187/api/v9/audits/?query=command%3D%3Dmkdirs&startTime=$previous_
 ate_epoch&endTime=$current_date_epoch&offset=0&format=$report_format&attachment=false" -o  audit_hdfs_mkdirs_`date +%F`.out
 ## With Limit option
 #/usr/bin/curl -k -u jay.kumar:India@123 "https://br1paprhdd11.passporthealth.com:7187/api/v9/audits/?query=command%3D%3DGRANT_PRIVILEGE&startTime=$previous_date
